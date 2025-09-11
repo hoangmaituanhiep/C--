a2db3aba50277eb60a02c4d3ac2e8585c7076264
Heip <toidungoutlook@gmail.com>
2025-09-11 14:42:10 +0700

    quicksort c++

---
+++ DSA/quicksort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define init ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+ll partition(vector<ll> array,ll low,ll high){
+    ll pivot = array[high];
+    ll index=low;
+
+    for (ll i= low; i<=high; ++i){
+        if (array[i]<=pivot){
+            ++index;
+        }
+    }
+    array[index] = array[index] ^ array[high];
+    array[high] = array[index] ^ array[high];
+    array[index] = array[index] ^ array[high];
+    return index;
+}
+
+void quicksort(vector<ll> array, ll low, ll high){
+    if (low< high){
+        ll mid = partition(array, low, high);
+        quicksort(array, mid+1, high);
+        quicksort(array, low, mid-1);
+    }
+}
+
+
+int main() {
+    init
+    ll n; cin>>n;
+    vector<ll> array(n);
+    for (ll i =0; i<n; ++i) cin>>array[i];
+
+    quicksort(array, 0, n-1);
+
+    for (ll i =0; i<n; ++i) cout<<array[i]<<" ";
+
+    return 0;
+}
