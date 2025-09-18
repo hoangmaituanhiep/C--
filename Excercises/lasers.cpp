97c0b28830c42e5bdc3f52a04396a23a88d96f7a
Heip <toidungoutlook@gmail.com>
2025-09-18 08:46:17 +0700

    new excercises

---
+++ Excercises/lasers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define init ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+//https://codeforces.com/problemset/problem/2148/B
+
+int main(){
+    init
+    return 0;
+}
