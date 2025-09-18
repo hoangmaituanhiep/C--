f84a50f61acc4f091cd9f36ebe6ad95394ca0120
Heip <toidungoutlook@gmail.com>
2025-09-18 10:02:54 +0700

    try but failed

--- Excercises/pacer.cpp
+++ Excercises/pacer.cpp
@@ -12,15 +12,24 @@ int main(){
 
     while(t--){
         ll n,m; cin>>n>>m;
-        vector <ll> a(n), b(n);
-        a[0] = 0; b[i]  = 0;
+        vector <ll> a(n+1), b(n+1);
+        a[0] = 0; b[0]  = 0;
         for (ll i=1; i<=n; ++i) cin>> a[i]>>b[i];
 
         ll dem=0;
+        ll index=0;
 
-        if (m>a[n-1]){
-            dem += m-a[n-1];
-        }
+        dem += m-a[n-1];
+        while (m=m-a[index] && index + 1 <=n){
+            if ((a[index+1] - a[index])%2 == (abs(b[index+1] - b[index]))){
+                dem+= a[index+1] - a[index];
+                }
+            else{
+                dem+= a[index+1] - a[index] - 1;
+            }
+            ++index;
+            }
+        cout<<dem<<'\n';
     }
     return 0;
 }
