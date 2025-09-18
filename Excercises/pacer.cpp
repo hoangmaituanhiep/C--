116497faca5b0c5f08f870e0351405fa00842145
Heip <toidungoutlook@gmail.com>
2025-09-18 09:29:02 +0700

    new excercises

---
+++ Excercises/pacer.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define init ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+//https://codeforces.com/problemset/problem/2148/C
+
+int main(){
+    init
+    int t; cin>>t;
+
+    while(t--){
+        ll n,m; cin>>n>>m;
+        vector <ll> a(n); for (ll i=0; i<n; ++i) cin>> a[i];
+    }
+    return 0;
+}
