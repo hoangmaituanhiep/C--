2c0896cf2dc79d75cfe0aba5cd72999175444d88
Heip <toidungoutlook@gmail.com>
2025-09-27 10:14:56 +0700

    Reorganize folder

--- DSA/LinkedLists/LinkedLists.cpp
+++ DSA/LinkedLists/LinkedLists.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-#define init ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define init ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
 int main() {
-    init;
+    init
     return 0;
 }
