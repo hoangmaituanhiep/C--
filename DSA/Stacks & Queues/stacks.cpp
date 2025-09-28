e01089c1d5cf3c5c96d7163ec0d9e6681b913ec6
Heip <toidungoutlook@gmail.com>
2025-09-28 20:30:44 +0700

    New data structure

---
+++ DSA/Stacks & Queues/stacks.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define init ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define maxn 1000000009
+
+class Stack{
+    public:
+        ll top = 0;
+        ll stack[maxn];
+
+        void push(ll n){
+            stack[top] = n;
+            ++top;
+        }
+
+        bool isEmpty(){
+            return top == 0;
+        }
+
+        void pop(){
+            if (isEmpty()){
+                cout<<'\nStack is empty!';
+            }
+            else{
+                --top;
+            }
+        }
+        void peek(){
+            if (isEmpty()){
+                cout<<'\nStack is empty!';
+            }
+            else{
+                cout<<'\n'<<stack[top];
+            }
+        }
+};
+
+int main() {
+    init
+    return 0;
+}
