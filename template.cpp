348867695104aa964ec9766f0c518829c81a244d
Heip <toidungoutlook@gmail.com>
2025-10-03 21:33:31 +0700

    Template

---
+++ template.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define init ios_base::sync_with_stdio(0);cin.tie(nullptr);cout.tie(0);
+
+//Optimze IO operations
+const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
+
+#define LC_HACK 
+const auto __ = []() {
+    struct ___ {
+        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
+    };
+    std::atexit(&___::_);
+    return 0;
+}();
+
+
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//            Phật phù hộ, không bao giờ BUG
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Start coding
+
+int main(){
+    init
+
+    return 0;
+}
