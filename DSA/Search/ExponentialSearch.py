d0bc39b279f90c9f34555aee8ee9d2e7b0776ba3
Heip <toidungoutlook@gmail.com>
2025-12-05 21:04:59 +0700

    Improve ExponentialSearch.py

--- DSA/Search/ExponentialSearch.py
+++ DSA/Search/ExponentialSearch.py
@@ -1,28 +1,28 @@
 import time
 
+def BS(target, low, high, array):
+    while low<high:
+        mid = (low+high)//2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            high = mid-1
+        else:
+            low = mid+1
+        
+    return -1
+
 def ES(array, k):
     if array[0] == k:
         return 0
     
     i = 1
-    pre = i
 
     while array[i] <= k and i < len(array):
-        pre = i
         i *= 2
 
-    low = pre
-    high = i
-    while low<high:
-        mid = (low+high)//2
-
-        if array[mid] == k:
-            return mid
-        elif array[mid]>k:
-            high = mid-1
-        else:
-            low = mid+1
-    return -1
+    return BS(k, i//2, i, array)
 
 def main():
     start = time.time()
