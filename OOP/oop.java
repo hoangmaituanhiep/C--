a28a7126992b863c13dbd5f72bfa10bf6793c8f9
Heip <toidungoutlook@gmail.com>
2026-02-08 14:35:07 +0700

    Main function

--- OOP/oop.java
+++ OOP/oop.java
@@ -1,11 +1,9 @@
-package OOP;
-
 public class oop {
-    String hi = "Oil car discord mess";
-    public static void main (String[] args) {
-        oop obj = new opp();
-        System.out.println(obj.hi);
+    private static final int limit = 6;
+
+    public static void main(String[] args){
+        temp user = new temp();
+        user.IQ_assess(limit);
+        user.func_call(limit);
     }
 }
-
-
