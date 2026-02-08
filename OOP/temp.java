832644cfc39fb992b021bd30f57198a343c39e83
Heip <toidungoutlook@gmail.com>
2026-02-08 14:34:41 +0700

    Programming with multiple files

---
+++ OOP/temp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class temp {
+    public void func_call(int times){
+        for (int i=1; i<times; i++){
+            System.out.println(i + ", Callin ...");
+        }
+    }
+
+    public void IQ_assess(int num){
+        Scanner line = new Scanner(System.in);
+
+        System.out.println("Out of ten. How intelligent are you?");
+        int iq = line.nextInt();
+
+        if (iq > num) {
+            System.out.println("You sure have some IQ");
+        }
+        else {
+            System.out.println("Such stupidity!");
+        }
+    }
+}
