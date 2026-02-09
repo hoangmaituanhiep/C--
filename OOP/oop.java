158b6c03d1985aea92ab564403036a39206ffd0f
Heip <toidungoutlook@gmail.com>
2026-02-09 11:48:26 +0700

    Learning Java this Keyword

--- OOP/oop.java
+++ OOP/oop.java
@@ -1,9 +1,28 @@
 public class oop {
-    private static final int limit = 6;
+    public static int limit = 6;
+    String user_name;
+    int tokens;
+
+    public oop(int new_limit) {
+        oop.limit = new_limit;
+
+        this("Gay Gun", 10);
+    }
+
+    public oop(String user_name, int tokens){
+        this.user_name = user_name;
+        this.tokens = tokens;
+    }
 
     public static void main(String[] args){
         temp user = new temp();
         user.IQ_assess(limit);
-        user.func_call(limit);
+
+        oop obj = new oop(10);
+
+        System.out.println(obj.user_name);
+        System.out.println("Number of tokens possesing: " + obj.tokens);
+
+        user.func_call(obj.tokens);
     }
 }
