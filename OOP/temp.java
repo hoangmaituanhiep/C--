946147d561a212dba86870fd1407ae206a6d1785
Heip <toidungoutlook@gmail.com>
2026-02-09 11:48:44 +0700

    Modifying for loop

--- OOP/temp.java
+++ OOP/temp.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class temp {
     public void func_call(int times){
-        for (int i=1; i<times; i++){
+        for (int i=1; i<=times; i++){
             System.out.println(i + ", Callin ...");
         }
     }
 
     public void IQ_assess(int num){
-        Scanner line = new Scanner(System.in);
+        try (Scanner line = new Scanner(System.in)) {
+            System.out.println("Out of ten. How intelligent are you?");
+            int iq = line.nextInt();
 
-        System.out.println("Out of ten. How intelligent are you?");
-        int iq = line.nextInt();
-
-        if (iq > num) {
-            System.out.println("You sure have some IQ");
-        }
-        else {
-            System.out.println("Such stupidity!");
+            if (iq > num) {
+                System.out.println("You sure have some IQ");
+            }
+            else {
+                System.out.println("Such stupidity!");
+            }
         }
     }
 }
