8ad7a36c74b501bc8a5a7b809c933249afea2147
Heip <toidungoutlook@gmail.com>
2026-02-20 19:58:11 +0700

    Optimize a little time and space complexity in Longest Common Subsequence

--- DSA/Array/lcs.cpp
+++ DSA/Array/lcs.cpp
@@ -18,12 +18,9 @@ int lcs(string x, string y){
     for (int i=0; i<=n1; i++){
         for (int j=0; j<=n2; j++){
 
-            if (i == 0 || j == 0) dp[i][j] = 0;
-
-            else{
-                if (x[i-1] == y[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
-            }
+            if (x[i-1] == y[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+            
+            else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
         }
     }
 
