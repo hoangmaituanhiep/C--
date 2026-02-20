558755a4a0d75e99abd99ad9db8adb771ccdb6e8
Heip <toidungoutlook@gmail.com>
2026-02-20 20:24:00 +0700

    Move dp files to DP folder

---
+++ DSA/Array/Dynamic Programming/lis.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define init ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define l <<'\n';
+
+vector<int> nums = {12, 4, 62, 2, 45, 75, 23, 55, 67, 34, 65};
+
+int lis(vector<int>& nums){
+    int n = nums.size();
+    vector<int> dp(n+1, 0);
+
+    for (int i=1; i<n; i++){
+
+    }
+
+    return dp[n];
+}
+
+int main()
+{
+    init
+
+    return 0;
+}
