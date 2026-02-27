dab82eb10ce5a7075eeb5ce1aa9d17ddfe68f2f2
Heip <toidungoutlook@gmail.com>
2026-02-27 07:50:00 +0700

    Doing OOP Exercise: The Authors and the Book

---
+++ OOP/Exercises/The Author and the Book/Author.java
@@ -0,0 +1,27 @@
+public class Author{
+    private String name;
+    private String email;
+    private char gender;
+
+    Author(String name, String email, char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public char getGender(){
+        return this.gender;
+    }
+
+    public String toString(){
+        return String.format("Author[name=%s,email=%s,gender=%s]", this.name, this.email, this.gender);
+    }
+}
